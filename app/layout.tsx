3d10935ffa8fb6c378d2007e7afdfe980e164d13
toffly <toffly@outlook.com>
2025-10-19 15:43:19 +0700

    fix container

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-poppins antialiased container">
+      <body className="font-poppins antialiased">
         <Navbar />
         {children}
         <Footer />
