7158f7d4d751839782321001dfc6cc96d65b01d0
toffly <toffly@outlook.com>
2025-10-28 12:28:04 +0700

    sanity and database

---
+++ app/(client)/page.tsx
@@ -0,0 +1,12 @@
+import HomeBanner from "@/components/HomeBanner";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="p-10 bg-shop-light-pink">
+      <HomeBanner />
+    </div>
+  );
+};
+
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
-export const metadata: Metadata = {
-  title: "Shopcard Online Store",
-  description: "Shopcard online store, Your one-stop shop for everything!",
-};
-
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased container">
-          <Navbar />
-          <div className="flex-1">{children}</div>
-          <Footer />
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className="font-poppins antialiased container">{children}</body>
+    </html>
   );
-}
+};
+
+export default RootLayout;
